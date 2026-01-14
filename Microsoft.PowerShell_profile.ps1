7e6b365b959a72a7e1c56798149909504cbad59b
IndianaHein <heinhammervold@gmail.com>
2026-01-14 14:54:42 +0100

    Publish PowerShell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -36,7 +36,7 @@ function Test-GitHubReachable {
 $script:CanConnectToGitHub = Test-GitHubReachable
 
 # Platform flags
-$IsWindowsPlatform = $IsWindows -or ($env:OS -eq 'Windows_NT')
+#$IsWindowsPlatform = $IsWindows -or ($env:OS -eq 'Windows_NT')
 # Detect interactive host (avoid server remote hosts)
 $IsInteractive = -not ($Host.Name -in @('ServerRemoteHost', 'ConsoleHost'))
 
